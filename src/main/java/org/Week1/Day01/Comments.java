69445f529028c782d84381b7c444054748772174
Mehmet Gul <mehmet@clarusway.com>
2022-12-23 20:30:39 -0500

    First Commit

---
+++ src/main/java/org/Week1/Day01/Comments.java
@@ -0,0 +1,24 @@
+package org.Week1.Day01;
+
+public class Comments {
+    // This is first type of comment starts with //
+    //This is comment
+
+    /*
+        This is my first line
+        And this is my second line
+        And this will be the third
+     */
+
+    /**
+     * This is another comment with multi-line
+     * this is second line
+     * Comment 3rd line
+     */
+
+    //TODO this is too message
+    //todo this is todo message in small and todo is not case sensitive
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
